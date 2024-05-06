78b93e2af3b2b27240581d3c751cd971db4d7c29
Cedric-Font <font.cedric38@gmail.com>
2024-05-06 14:40:29 +0200

    fix navMobile

---
+++ ATpartners/frontend/src/form/Form.jsx
@@ -0,0 +1,11 @@
+import styles from "./Form.module.css";
+import NavMobile from "../navBarMobile/NavMobile";
+
+export default function Form() {
+
+    return(
+        <>
+        <NavMobile />
+        </>
+    )
+}
