b3c8a2960f46c9215d4209e70db5900466e8e85e
Cedric-Font <font.cedric38@gmail.com>
2024-05-27 21:42:28 +0200

    finish

--- ATpartners/frontend/src/aboutATservices/AboutATservices.jsx
+++ ATpartners/frontend/src/aboutATservices/AboutATservices.jsx
@@ -2,26 +2,26 @@ import styles from "./AboutATservices.module.css";
 import ATservicesMain from "../assets/AtservicesHeader.jpg";
 import SubTitle from "../SubTitle/SubTitle";
 import NavMobile from "../navBarMobile/NavMobile";
+import OurValues from "../OurValues/OurValues";
 
 export default function AboutATservices() {
-    
-    const objectifsTab = [
-        {
-            number: "1.",
-            text: "• Fidéliser et développer notre clientèle"
-        },
-        {
-            number: "2.",
-            text: "• Ouvrir une succursale dans le sud de la France pour avoir une plus grande mobilité des équipes et un contact plus étroit avec les clients."
-        },
-        {
-            number: "3.",
-            text: "• Se développer dans toute la communauté de l’UE. Pour atteindre ces objectifs, nous plaçons la satisfaction du client au cœur de nos priorités. Cela passe, entre autres, par une écoute permanente et une qualité d’exécution quotidienne."
-        }
-    ]
+  const objectifsTab = [
+    {
+      number: "1.",
+      text: "• Fidéliser et développer notre clientèle",
+    },
+    {
+      number: "2.",
+      text: "• Ouvrir une succursale dans le sud de la France pour avoir une plus grande mobilité des équipes et un contact plus étroit avec les clients.",
+    },
+    {
+      number: "3.",
+      text: "• Se développer dans toute la communauté de l’UE. Pour atteindre ces objectifs, nous plaçons la satisfaction du client au cœur de nos priorités. Cela passe, entre autres, par une écoute permanente et une qualité d’exécution quotidienne.",
+    },
+  ];
   return (
     <>
-    <NavMobile />
+      <NavMobile />
       <section className={styles.AboutATservicesHeaderSection}>
         <div className={styles.mainPictureContainer}>
           <img src={ATservicesMain} alt="" className={styles.mainPicture} />
@@ -45,21 +45,33 @@ export default function AboutATservices() {
             création en 2008 et devient une entreprise incontournable du
             secteur.
           </p>
-          <p className={styles.objectifText}>Les objectifs de développement ont été atteints chaque
-            année et la société se place déjà parmi les entreprises française
-            les plus connues et appréciées dans le domaine de montage de
-            panneaux et menuiseries isothermes. Pour les prochaines années, nos
-            objectifs sont de:</p>
+          <p className={styles.objectifText}>
+            Les objectifs de développement ont été atteints chaque année et la
+            société se place déjà parmi les entreprises française les plus
+            connues et appréciées dans le domaine de montage de panneaux et
+            menuiseries isothermes. Pour les prochaines années, nos objectifs
+            sont de:
+          </p>
         </div>
         <div className={styles.mapContainer}>
-            {objectifsTab.map((objectif, index) => {
-                return(
-                    <div className={styles.objectifContainer} key={index}>
-                        <div className={styles.objectifNumber}>{objectif.number}</div>
-                        <p className={styles.objectifParg}>{objectif.text}</p>
-                         </div>
-                )
-            })}
+          {objectifsTab.map((objectif, index) => {
+            return (
+              <div className={styles.objectifContainer} key={index}>
+                <div className={styles.objectifNumber}>{objectif.number}</div>
+                <p className={styles.objectifParg}>{objectif.text}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className={styles.valueContainer}>
+            <OurValues />
+          {/* <h2 className={styles.valueTitle}>NOS VALEURS ET ENGAGEMENTS</h2>
+          <p className={styles.valueText}>
+            La société Valentin Espace de Vie est animée au quotidien autour de
+            4 valeurs que l’ensemble des collaborateurs partagent. Ces
+            engagements nous permettent de réaliser vos projets en toute
+            sérénité et d’assurer votre satisfaction.
+          </p> */}
         </div>
       </section>
     </>
